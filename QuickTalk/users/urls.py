a1336d7c5b3f9b62beba4451a1eda7ae032ff2d0
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-12 23:21:22 +0300

    changed 'name' parameter in URL-s for API-conf to divide default front pages from API pages

--- QuickTalk/users/urls.py
+++ QuickTalk/users/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import CustomUserLoginAPIView, CustomUserCreateAPIView
 
 urlpatterns = [
-    path('login/', CustomUserLoginAPIView.as_view(), name='login'),
-    path('register/', CustomUserCreateAPIView.as_view(), name='register')
+    path('login/', CustomUserLoginAPIView.as_view(), name='api-login'),
+    path('register/', CustomUserCreateAPIView.as_view(), name='api-register')
 ]
