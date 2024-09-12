c09294ff30114baedc8cb1b76316fe29ecbb9c8d
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-12 23:18:39 +0300

    added 'password2' field with validation to confirm password during registration

--- QuickTalk/users/serializers.py
+++ QuickTalk/users/serializers.py
@@ -7,12 +7,21 @@ from phonenumber_field.serializerfields import PhoneNumberField
 
 class RegisterCustomUserSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password2 = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
 
     class Meta:
         model = CustomUser
-        fields = ['phone_number', 'password']
+        fields = ['phone_number', 'password', 'password2']
+
+    def validate(self, data):
+        # Проверяем, что два пароля совпадают
+        if data.get('password') != data.get('password2'):
+            raise serializers.ValidationError({"password": "Passwords do not match."})
+        return data
 
     def create(self, validated_data):
+        # Удаляем поле password2, так как оно не нужно для создания пользователя
+        validated_data.pop('password2')
         user = CustomUser(
             phone_number=validated_data.get('phone_number'),
         )
