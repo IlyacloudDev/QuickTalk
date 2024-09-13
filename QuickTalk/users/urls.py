a9dcabc71784b3d40d638c3c7c49f320edb5324a
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-13 23:23:15 +0300

    added path to API-conf for updating user's profile

--- QuickTalk/users/urls.py
+++ QuickTalk/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CustomUserLoginAPIView, CustomUserCreateAPIView
+from .views import CustomUserLoginAPIView, CustomUserCreateAPIView, CustomUserUpdateAPIView
 
 urlpatterns = [
     path('login/', CustomUserLoginAPIView.as_view(), name='api-login'),
-    path('register/', CustomUserCreateAPIView.as_view(), name='api-register')
+    path('register/', CustomUserCreateAPIView.as_view(), name='api-register'),
+    path('update-user/<int:pk>/', CustomUserUpdateAPIView.as_view(), name='api-update-user')
 ]
