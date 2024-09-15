659d8e0822b4557b1c9e273a8fda395fc7fe2db4
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-16 00:29:50 +0300

    added path to API-conf for signing out from user's profile

--- QuickTalk/users/urls.py
+++ QuickTalk/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CustomUserLoginAPIView, CustomUserCreateAPIView, CustomUserUpdateAPIView
+from .views import CustomUserLoginAPIView, CustomUserCreateAPIView, CustomUserLogoutAPIView, CustomUserUpdateAPIView
 
 urlpatterns = [
     path('login/', CustomUserLoginAPIView.as_view(), name='api-login'),
     path('register/', CustomUserCreateAPIView.as_view(), name='api-register'),
+    path('logout/', CustomUserLogoutAPIView.as_view(), name='api-logout'),
     path('update-user/<int:pk>/', CustomUserUpdateAPIView.as_view(), name='api-update-user')
 ]
