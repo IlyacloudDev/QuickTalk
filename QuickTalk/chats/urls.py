ddef79fe3973905c78411a83796123e4cc39dce7
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-21 18:56:59 +0300

    added path to API-conf for output list of user chats

--- QuickTalk/chats/urls.py
+++ QuickTalk/chats/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CreateGroupChatAPIView
+from .views import CreateGroupChatAPIView, ChatsListAPIView
 
 
 urlpatterns = [
     path('create-group-chat/', CreateGroupChatAPIView.as_view(), name='api-create-group-chat'),
+    path('chats-list/', ChatsListAPIView.as_view(), name='api-chats-list'),
 ]
