e3ebf6e79786f8a979a85b22cd75ea4da0d4bbcf
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-23 01:06:21 +0300

    added path to API-conf for output detail of user profile

--- QuickTalk/users/urls.py
+++ QuickTalk/users/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import CustomUserLoginAPIView, CustomUserCreateAPIView, CustomUserLogoutAPIView, CustomUserUpdateAPIView, CustomUserSearchAPIView
+from .views import CustomUserLoginAPIView, CustomUserCreateAPIView, CustomUserLogoutAPIView, CustomUserUpdateAPIView, CustomUserSearchAPIView, CustomUserDetailAPIView
 
 urlpatterns = [
     path('login/', CustomUserLoginAPIView.as_view(), name='api-login'),
     path('register/', CustomUserCreateAPIView.as_view(), name='api-register'),
     path('logout/', CustomUserLogoutAPIView.as_view(), name='api-logout'),
     path('update-user/<int:pk>/', CustomUserUpdateAPIView.as_view(), name='api-update-user'),
-    path('search-user/', CustomUserSearchAPIView.as_view(), name='api-search-user')
+    path('search-user/', CustomUserSearchAPIView.as_view(), name='api-search-user'),
+    path('detail-user/<int:pk>/', CustomUserDetailAPIView.as_view(), name='api-detail-user'),
 ]
