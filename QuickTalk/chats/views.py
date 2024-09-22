575d958eab15d940cd8f17df9d6d8bed499a2887
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-23 00:52:26 +0300

    added view for creating personal chats; modified view for output list of user chats

--- QuickTalk/chats/views.py
+++ QuickTalk/chats/views.py
@@ -2,7 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import IsAuthenticated
-from .serializers import CreateGroupChatSerializer, ChatsListSerializer
+from .serializers import CreateGroupChatSerializer, ChatsListSerializer, CreatePersonalChatSerializer
 from .models import Chat
 
 
@@ -18,11 +18,31 @@ class CreateGroupChatAPIView(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class CreatePersonalChatAPIView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, *args, **kwargs):
+        # Создаем экземпляр сериализатора с данными из запроса
+        serializer = CreatePersonalChatSerializer(data=request.data, context={'request': request})
+        
+        if serializer.is_valid():
+            chat = serializer.save()
+            return Response({
+                'detail': 'Chat created successfully',
+                'chat_id': CreatePersonalChatSerializer(chat).data
+            }, status=status.HTTP_201_CREATED)
+        
+        # Если валидация не прошла, отправляем ошибки
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
 class ChatsListAPIView(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, *args, **kwargs):
         user = request.user
-        chats = Chat.objects.filter(users=user)
-        serializer = ChatsListSerializer(chats, many=True)
+        chats = Chat.objects.filter(users=user).order_by('-created_at')
+        serializer = ChatsListSerializer(chats, many=True, context={'request': request})
         return Response(serializer.data)
+    
