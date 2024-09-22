a91c2492689fb76b565f566c34ba6461236950a3
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-23 00:50:51 +0300

    added path to API-conf for creating personal chats

--- QuickTalk/chats/urls.py
+++ QuickTalk/chats/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CreateGroupChatAPIView, ChatsListAPIView
+from .views import CreateGroupChatAPIView, ChatsListAPIView, CreatePersonalChatAPIView
 
 
 urlpatterns = [
     path('create-group-chat/', CreateGroupChatAPIView.as_view(), name='api-create-group-chat'),
     path('chats-list/', ChatsListAPIView.as_view(), name='api-chats-list'),
+    path('create-personal-chat', CreatePersonalChatAPIView.as_view(), name='api-create-personal-chat'),
 ]
