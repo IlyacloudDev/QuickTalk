43ef93e94bd12976473fdfafaa01d6217cbf8718
Ilya Stritskiy <msv_ilya@mail.ru>
2024-09-26 23:43:20 +0300

    changed the URL configuration for the correct operation of the daphne server

--- QuickTalk/config/urls.py
+++ QuickTalk/config/urls.py
@@ -39,3 +39,6 @@ urlpatterns = [
     path('api/users/', include('users.urls')),
     path('api/chats/', include('chats.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
